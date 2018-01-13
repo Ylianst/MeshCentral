501845e412464d3d93b7ea6e7fbbaf1876278c0f
Lawrence <technoexpressnet@gmail.com>
2018-01-13 13:18:00 -0500

    fix runtime errors

--- docker/startmeshcentral.sh
+++ docker/startmeshcentral.sh
@@ -2,11 +2,11 @@
 export PORT  
 export REDIRPORT
 
-if [ $1 != "" ] ; then
+if [ "$1" != "" ] ; then
     PORT=$1
 fi
 
-if [ $2 != "" ]; then
+if [ "$2" != "" ]; then
     REDIRPORT=$2
 fi
 
@@ -16,11 +16,7 @@ npm install meshcentral
 
 if ! [ -f node_modules/.meshcentral-data/agentserver-cert-private.key ] ;then 
 	forever start node_modules/meshcentral/meshcentral.js --cert $HOSTNAME
-	sleep 10
-	forever stop node_modules/meshcentral/meshcentral.js
-fi
-
-if [ -f ssl.key ]; then
+elif [ -f ssl.key ]; then
     ln -sf ssl.key node_modules/.meshcentral-data/agentserver-cert-private.key  
     ln -sf ssl.cert node_modules/.meshcentral-data/agentserver-cert-public.crt
     ln -sf ssl.key node_modules/.meshcentral-data/root-cert-private.key   
@@ -29,7 +25,7 @@ if [ -f ssl.key ]; then
     ln -sf ssl.cert node_modules/.meshcentral-data/webserver-cert-public.crt
     ln -sf ssl.key node_modules/.meshcentral-data/mpsserver-cert-private.key 
     ln -sf ssl.cert node_modules/.meshcentral-data/mpsserver-cert-public.crt
+else
+	forever start node_modules/meshcentral/meshcentral.js --port $PORT --redirport $REDIRPORT
 fi
-
-forever start node_modules/meshcentral/meshcentral.js --port $PORT --redirport $REDIRPORT
  
--- startmeshcentral.sh
+++ startmeshcentral.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
-if [ $1 == "" ] ; then
+if [ "$1" == "" ] ; then
     PORT=444
 else
     PORT=$1
 fi
 
-if [ $2 == "" ]; then
-    RPORT=81
+if [ "$2" == "" ]; then
+    REDIRPORT=81
 else
-    RPORT=$2
-fi
-
-
-if [ $1 != "" ] ; then
-    PORT=$1
-fi
-
-if [ $2 != "" ]; then
     REDIRPORT=$2
 fi
 
@@ -25,12 +16,8 @@ cd /home/meshcentral/
 npm install meshcentral
 
 if ! [ -f node_modules/.meshcentral-data/agentserver-cert-private.key ] ;then 
-	forever start node_modules/meshcentral/meshcentral.js --cert $HOSTNAME
-	sleep 10
-	forever stop node_modules/meshcentral/meshcentral.js
-fi
-
-if [ -f ssl.key ]; then
+	forever start node_modules/meshcentral/meshcentral.js --cert $HOSTNAME --port $PORT --redirport $REDIRPORT
+elif [ -f ssl.key ]; then
     ln -sf ssl.key node_modules/.meshcentral-data/agentserver-cert-private.key  
     ln -sf ssl.cert node_modules/.meshcentral-data/agentserver-cert-public.crt
     ln -sf ssl.key node_modules/.meshcentral-data/root-cert-private.key   
@@ -38,8 +25,9 @@ if [ -f ssl.key ]; then
     ln -sf ssl.key node_modules/.meshcentral-data/webserver-cert-private.key   
     ln -sf ssl.cert node_modules/.meshcentral-data/webserver-cert-public.crt
     ln -sf ssl.key node_modules/.meshcentral-data/mpsserver-cert-private.key 
-    ln -sf ssl.cert node_modules/.meshcentral-data/mpsserver-cert-public.crt
+    ln -sf ssl.cert node_modules/.meshcentral-data/mpsserver-cert-public.crt	
+	forever start node_modules/meshcentral/meshcentral.js --port $PORT --redirport $REDIRPORT
+else
+	forever start node_modules/meshcentral/meshcentral.js --port $PORT --redirport $REDIRPORT
 fi
-
-forever start node_modules/meshcentral/meshcentral.js --port $PORT --redirport $REDIRPORT
  
