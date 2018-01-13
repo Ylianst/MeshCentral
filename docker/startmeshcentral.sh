9efb148f8fdc1103080cdbbd97a174560ba32836
Lawrence <technoexpressnet@gmail.com>
2018-01-13 15:48:35 -0500

    setup container to use https://github.com/adi90x/rancher-active-proxy for certs and proxy

--- docker/startmeshcentral.sh
+++ docker/startmeshcentral.sh
@@ -10,22 +10,22 @@ if [ "$2" != "" ]; then
     REDIRPORT=$2
 fi
 
-su - meshcentral
-cd /home/meshcentral/
+su - meshserver
+cd /home/meshserver/
 npm install meshcentral
 
-if ! [ -f node_modules/.meshcentral-data/agentserver-cert-private.key ] ;then 
-	node node_modules/meshcentral/meshcentral.js --cert $HOSTNAME --port $PORT --redirport $REDIRPORT
-elif [ -f ssl.key ]; then
-    ln -sf ssl.key node_modules/.meshcentral-data/agentserver-cert-private.key  
-    ln -sf ssl.cert node_modules/.meshcentral-data/agentserver-cert-public.crt
-    ln -sf ssl.key node_modules/.meshcentral-data/root-cert-private.key   
-    ln -sf ssl.cert node_modules/.meshcentral-data/root-cert-public.crt     
-    ln -sf ssl.key node_modules/.meshcentral-data/webserver-cert-private.key   
-    ln -sf ssl.cert node_modules/.meshcentral-data/webserver-cert-public.crt
-    ln -sf ssl.key node_modules/.meshcentral-data/mpsserver-cert-private.key 
-    ln -sf ssl.cert node_modules/.meshcentral-data/mpsserver-cert-public.crt
+if [ -f "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" ]; then
+    ln -sf "/etc/letsencrypt/archive/$HOSTNAME/privkey1.pem" meshcentral-data/agentserver-cert-private.key  
+    ln -sf "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" meshcentral-data/agentserver-cert-public.crt
+    ln -sf "/etc/letsencrypt/archive/$HOSTNAME/privkey1.pem" meshcentral-data/root-cert-private.key   
+    ln -sf "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" meshcentral-data/root-cert-public.crt     
+    ln -sf "/etc/letsencrypt/archive/$HOSTNAME/privkey1.pem" meshcentral-data/webserver-cert-private.key   
+    ln -sf "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" meshcentral-data/webserver-cert-public.crt
+    ln -sf "/etc/letsencrypt/archive/$HOSTNAME/privkey1.pem" meshcentral-data/mpsserver-cert-private.key 
+    ln -sf "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" meshcentral-data/mpsserver-cert-public.crt
 	node node_modules/meshcentral/meshcentral.js --port $PORT --redirport $REDIRPORT
+elif ! [ -f meshcentral-data/agentserver-cert-private.key ] ;then 
+	node node_modules/meshcentral/meshcentral.js --cert $HOSTNAME --port $PORT --redirport $REDIRPORT
 else
 	node node_modules/meshcentral/meshcentral.js --port $PORT --redirport $REDIRPORT
 fi
--- startmeshcentral.sh
+++ startmeshcentral.sh
@@ -11,23 +11,22 @@ else
     REDIRPORT=$2
 fi
 
-
-su - meshcentral
-cd /home/meshcentral/
+su - meshserver
+cd /home/meshserver/
 npm install meshcentral
 
-if ! [ -f node_modules/.meshcentral-data/agentserver-cert-private.key ] ;then 
-	forever start node_modules/meshcentral/meshcentral.js --cert $HOSTNAME --port $PORT --redirport $REDIRPORT
-elif [ -f ssl.key ]; then
-    ln -sf ssl.key node_modules/.meshcentral-data/agentserver-cert-private.key  
-    ln -sf ssl.cert node_modules/.meshcentral-data/agentserver-cert-public.crt
-    ln -sf ssl.key node_modules/.meshcentral-data/root-cert-private.key   
-    ln -sf ssl.cert node_modules/.meshcentral-data/root-cert-public.crt     
-    ln -sf ssl.key node_modules/.meshcentral-data/webserver-cert-private.key   
-    ln -sf ssl.cert node_modules/.meshcentral-data/webserver-cert-public.crt
-    ln -sf ssl.key node_modules/.meshcentral-data/mpsserver-cert-private.key 
-    ln -sf ssl.cert node_modules/.meshcentral-data/mpsserver-cert-public.crt	
+if [ -f ssl.key ]; then
+    ln -sf ssl.key meshcentral-data/agentserver-cert-private.key  
+    ln -sf ssl.cert meshcentral-data/agentserver-cert-public.crt
+    ln -sf ssl.key meshcentral-data/root-cert-private.key   
+    ln -sf ssl.cert meshcentral-data/root-cert-public.crt     
+    ln -sf ssl.key meshcentral-data/webserver-cert-private.key   
+    ln -sf ssl.cert meshcentral-data/webserver-cert-public.crt
+    ln -sf ssl.key meshcentral-data/mpsserver-cert-private.key 
+    ln -sf ssl.cert meshcentral-data/mpsserver-cert-public.crt
 	forever start node_modules/meshcentral/meshcentral.js --port $PORT --redirport $REDIRPORT
+elif ! [ -f meshcentral-data/agentserver-cert-private.key ] ;then 
+	forever start node_modules/meshcentral/meshcentral.js --cert $HOSTNAME --port $PORT --redirport $REDIRPORT
 else
 	forever start node_modules/meshcentral/meshcentral.js --port $PORT --redirport $REDIRPORT
 fi
