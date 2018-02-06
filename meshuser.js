ec787aa0eae9a74b35935bb55d2a00958e6b6f35
techno-express <technoexpressnet@gmail.com>
2018-02-05 20:29:42 -0500

    updated 7zip sfx module, code embed in module

--- meshuser.js
+++ meshuser.js
@@ -19,10 +19,7 @@ module.exports.CreateMeshUser = function (parent, db, ws, req, args, domain) {
     obj.parent = parent;
     obj.domain = domain;
     obj.common = parent.common;     
-    var Zip = require('node-7z-forall');  
-    var macos = (process.platform == "darwin") ? require('macos-release').version : '';
-    var binarypath = obj.path.resolve(__dirname, '..', 'node-7z-forall','binaries', (macos=='') ? process.platform : process.platform, macos );
-    var exePath = { path: obj.path.join(binarypath, process.platform === "win32" ? '7za.exe' : '7za' );    
+    var Zip = require('node-7z-forall');   
 // Create windows sfx mesh agent    
     function createSfxMeshAgent(mesh, sfxmeshfile) {
         var sfxmodule = '7zS2.sfx'; 
@@ -35,13 +32,13 @@ module.exports.CreateMeshUser = function (parent, db, ws, req, args, domain) {
 		var sfxagentextun = obj.path.join(__dirname, 'agents', 'meshuninstaller.bat' );
 		var sfxagentextreg = obj.path.join(__dirname, 'agents', 'meshagent.reg' );
 		var sfxagentextreg64 = obj.path.join(__dirname, 'agents', 'meshagent64.reg' );
-		makesfx.add( sfxagent, [ sfxagentext, sfxagentextun, sfxagentextreg, sfxagentextreg64, sfxagent32bit, sfxagent64bit, sfxmeshfile ],  exePath, { sfx: sfxmodule } )
+		makesfx.add( sfxagent, [ sfxagentext, sfxagentextun, sfxagentextreg, sfxagentextreg64, sfxagent32bit, sfxagent64bit, sfxmeshfile ], { sfx: sfxmodule } )
 			.then(function () {
 				mesh.path = sfxagent;
 				mesh.filename = 'remotesupport_' + sfx_ext;
 				sfxagent = obj.path.join(__dirname, 'sfxagents', 'uninstallremotesupport_' + sfx_ext);
 				sfxagentext = obj.path.join(__dirname, 'agents', 'meshuninstall.bat' );
-				makesfx.add( sfxagent , [ sfxagentext ,sfxagent32bit, sfxagent64bit, sfxmeshfile ], exePath, { sfx: sfxmodule } )
+				makesfx.add( sfxagent , [ sfxagentext ,sfxagent32bit, sfxagent64bit, sfxmeshfile ], { sfx: sfxmodule } )
 					.then(function () {
 						mesh.path2 = sfxagent;
 						mesh.filename2 = 'uninstallremotesupport_' + sfx_ext;
@@ -569,7 +566,7 @@ module.exports.CreateMeshUser = function (parent, db, ws, req, args, domain) {
                                     var makesfx = new Zip();
                                     var tempdir = obj.path.join(__dirname, 'tmp' );
                                     // extract current mesh policy from Sfx                             
-                                    makesfx.extract( mesh.path2, tempdir, exePath )
+                                    makesfx.extract( mesh.path2, tempdir )
                                         .then(function () {
                                             // Delete current mesh SFX files
                                             if (mesh.path1 != null)
