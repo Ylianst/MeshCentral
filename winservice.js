d28454c5ec81e51888962a8ec96054f2f3df3899
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2018-03-14 15:16:55 -0700

    Fixed MeshCentral running as a Windows service.

---
+++ winservice.js
@@ -0,0 +1,11 @@
+/**
+* @description MeshCentral main module
+* @author Ylian Saint-Hilaire
+* @copyright Intel Corporation 2018
+* @license Apache-2.0
+* @version v0.0.1
+*/
+
+// This module is only called when MeshCentral is running as a Windows service.
+// In this case, we don't want to start a child process, so we launch directly without arguments.
+require('./meshcentral.js').mainStart({ "launch": true });
