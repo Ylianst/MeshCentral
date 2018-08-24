f6b6fe95066f849d2d880f17d5c857e80690f5aa
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2018-08-24 13:14:20 -0700

    Fixed agent multicast, new nominify url option.

--- public/compress.bat
+++ public/compress.bat
@@ -1,6 +1,11 @@
 @ECHO OFF
-REM *** default.handlebars
+REM *** Remove all old minified files
 DEL ..\views\default-min.handlebars
+DEL ..\views\default-mobile-min.handlebars
+DEL ..\views\login-min.handlebars
+DEL ..\views\login-mobile-min.handlebars
+
+REM *** default.handlebars
 COPY ..\views\default.handlebars index.html
 ..\..\WebSiteCompiler\bin\Debug\WebSiteCompiler.exe compress.wcc -c
 COPY compress.htm ..\views\default-min.handlebars
@@ -8,7 +13,6 @@ DEL compress.htm
 DEL index.html
 
 REM *** default-mobile.handlebars
-DEL ..\views\default-mobile-min.handlebars
 COPY ..\views\default-mobile.handlebars index.html
 ..\..\WebSiteCompiler\bin\Debug\WebSiteCompiler.exe compress.wcc -c
 COPY compress.htm ..\views\default-mobile-min.handlebars
@@ -16,7 +20,6 @@ DEL compress.htm
 DEL index.html
 
 REM *** login.handlebars
-DEL ..\views\login-min.handlebars
 COPY ..\views\login.handlebars index.html
 ..\..\WebSiteCompiler\bin\Debug\WebSiteCompiler.exe compress.wcc -c
 COPY compress.htm ..\views\login-min.handlebars
@@ -24,7 +27,6 @@ DEL compress.htm
 DEL index.html
 
 REM *** login-mobile.handlebars
-DEL ..\views\login-mobile-min.handlebars
 COPY ..\views\login-mobile.handlebars index.html
 ..\..\WebSiteCompiler\bin\Debug\WebSiteCompiler.exe compress.wcc -c
 COPY compress.htm ..\views\login-mobile-min.handlebars
