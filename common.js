b66e74766f08df7cd54d458557dec3cb96c5a2b7
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2018-12-20 14:14:37 -0800

    Added password requirements checking.

--- common.js
+++ common.js
@@ -153,3 +153,22 @@ module.exports.validateStrArray = function (array, minlen, maxlen) { if (((array
 module.exports.validateObject = function (obj) { return ((obj != null) && (typeof obj == 'object')); };
 module.exports.validateEmail = function (email, minlen, maxlen) { if (module.exports.validateString(email, minlen, maxlen) == false) return false; var emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; return emailReg.test(email); };
 module.exports.validateUsername = function (username, minlen, maxlen) { return (module.exports.validateString(username, minlen, maxlen) && (username.indexOf(' ') == -1)); };
+
+// Check password requirements
+module.exports.checkPasswordRequirements = function(password, requirements) {
+    if ((requirements == null) || (requirements == '') || (typeof requirements != 'object')) return true;
+    if (requirements.min) { if (password.length < requirements.min) return false; }
+    if (requirements.max) { if (password.length > requirements.max) return false; }
+    var num = 0, lower = 0, upper = 0, nonalpha = 0;
+    for (var i = 0; i < password.length; i++) {
+        if (/\d/.test(password[i])) { num++; }
+        if (/[a-z]/.test(password[i])) { lower++; }
+        if (/[A-Z]/.test(password[i])) { upper++; }
+        if (/\W/.test(password[i])) { nonalpha++; }
+    }
+    if (requirements.num && (num < requirements.num)) return false;
+    if (requirements.lower && (lower < requirements.lower)) return false;
+    if (requirements.upper && (upper < requirements.upper)) return false;
+    if (requirements.nonalpha && (nonalpha < requirements.nonalpha)) return false;
+    return true;
+}
