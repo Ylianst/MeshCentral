98dbbfde25b8f5fafafd62ac3af17b0e1bd20870
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2019-12-19 12:10:50 -0800

    Updated Dutch, improved back button in web app.

--- meshuser.js
+++ meshuser.js
@@ -412,7 +412,9 @@ module.exports.CreateMeshUser = function (parent, db, ws, req, args, domain, use
                     if ((user.siteadmin & 21) == 0) return; // Only site administrators with "site backup" or "site restore" or "site update" permissions can use this.
                     if (common.validateInt(command.hours, 0, 24 * 30) == false) return;
                     db.GetServerStats(command.hours, function (err, docs) {
-                        if (err == null) { ws.send(JSON.stringify({ action: 'servertimelinestats', events: docs })); }
+                        if (err == null) {
+                            try { ws.send(JSON.stringify({ action: 'servertimelinestats', events: docs })); } catch (ex) { }
+                        }
                     });
                     break;
                 }
