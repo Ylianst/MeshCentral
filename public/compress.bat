bd1396bf7e5433645d3cecb8bb79cf613950ae9a
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2019-12-27 21:39:14 -0800

    Fixed toolbar in non-english languages.

--- public/compress.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-CD ..\translate
-C:\Users\Default.DESKTOP-M9I88C9\AppData\Roaming\nvm\v12.13.0\node translate.js minifyall
-C:\Users\Default.DESKTOP-M9I88C9\AppData\Roaming\nvm\v12.13.0\node translate.js translateall
