8275ddd0c997cba111a802a0a39aec51d384da72
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2020-01-22 16:26:42 -0800

    Updated MeshAgents with TLS1.3 and many more fixes.

--- public/translate.bat
+++ public/translate.bat
@@ -3,4 +3,3 @@ CD ..\translate
 C:\Users\Default.DESKTOP-M9I88C9\AppData\Roaming\nvm\v12.13.0\node translate.js minifyall
 C:\Users\Default.DESKTOP-M9I88C9\AppData\Roaming\nvm\v12.13.0\node translate.js translateall
 C:\Users\Default.DESKTOP-M9I88C9\AppData\Roaming\nvm\v12.13.0\node translate.js extractall
-pause
