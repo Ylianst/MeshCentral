9b239893f718cc8b89989e8555f064b383a1bdac
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2020-01-29 15:29:15 -0800

    Improved working directory switch.

--- meshcentral.js
+++ meshcentral.js
@@ -2281,9 +2281,8 @@ function mainStart() {
     // Check the NodeJS is version 6 or better.
     if (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 6) { console.log("MeshCentral requires Node v6 or above, current version is " + process.version + "."); return; }
 
-    // If running within the node_modules folder, move working directory back to parent.
-    if (process.cwd().endsWith('\\node_modules\\meshcentral') || process.cwd().endsWith('/node_modules/meshcentral')) { process.chdir(require('path').join(process.cwd(), '..', '..')); }
-    if (process.cwd().endsWith('\\node_modules') || process.cwd().endsWith('/node_modules')) { process.chdir(require('path').join(process.cwd(), '..')); }
+    // If running within the node_modules folder, move working directory to the parent of the node_modules folder.
+    if (__dirname.endsWith('\\node_modules\\meshcentral') || __dirname.endsWith('/node_modules/meshcentral')) { process.chdir(require('path').join(__dirname, '..', '..')); }
 
     // Check for any missing modules.
     InstallModules(['minimist'], function () {
