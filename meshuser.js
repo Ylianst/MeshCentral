11576ca5f31298f4408f56bb36ab3656f21f3491
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2020-04-30 18:52:45 -0700

    Fixed agent invite codes to make them unique per domain.

--- meshuser.js
+++ meshuser.js
@@ -2457,7 +2457,7 @@ module.exports.CreateMeshUser = function (parent, db, ws, req, args, domain, use
                                     var dup = null;
                                     for (var i in command.invite.codes) {
                                         for (var j in parent.meshes) {
-                                            if ((j != command.meshid) && (parent.meshes[j].invite != null) && (parent.meshes[j].invite.codes.indexOf(command.invite.codes[i]) >= 0)) { dup = command.invite.codes[i]; break; }
+                                            if ((j != command.meshid) && (parent.meshes[j].domain == domain.id) && (parent.meshes[j].invite != null) && (parent.meshes[j].invite.codes.indexOf(command.invite.codes[i]) >= 0)) { dup = command.invite.codes[i]; break; }
                                         }
                                     }
                                     if (dup != null) {
