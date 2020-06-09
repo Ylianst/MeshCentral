bdaa2f701373d4fa8dda991be86e43f8827d81fa
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2020-06-09 12:03:30 -0700

    Added option to remove noVNC link

--- webserver.js
+++ webserver.js
@@ -2156,6 +2156,7 @@ module.exports.CreateWebServer = function (parent, db, args, certificates) {
             if ((parent.smsserver != null) && ((typeof domain.passwordrequirements != 'object') || (domain.passwordrequirements.sms2factor != false))) { features += 0x04000000; } // SMS 2FA is allowed
             if (domain.sessionrecording != null) { features += 0x08000000; } // Server recordings enabled
             if (domain.urlswitching === false) { features += 0x10000000; } // Disables the URL switching feature
+            if (domain.novnc === false) { features += 0x20000000; } // Disables noVNC
 
             // Create a authentication cookie
             const authCookie = obj.parent.encodeCookie({ userid: user._id, domainid: domain.id, ip: req.clientIp }, obj.parent.loginCookieEncryptionKey);
