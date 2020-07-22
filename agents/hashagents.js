58cee052c3cab4c5082d8d2450710671aa8fa4c8
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2020-07-22 12:18:34 -0700

    Added agent hashing batch file.

---
+++ agents/hashagents.js
@@ -0,0 +1,29 @@
+var fs = require('fs'), r = '';
+
+var agents = {
+    'MeshService-signed.exe': 3,
+    'MeshService64-signed.exe': 4,
+    'meshagent_x86': 5,
+    'meshagent_x86-64': 6,
+    'meshagent_arm': 9,
+    'meshagent_mips': 7,
+    'meshagent_pogo': 13,
+    'meshagent_poky': 15,
+    'meshagent_osx-x86-64': 16,
+    'meshagent_poky64': 18,
+    'meshagent_x86_nokvm': 19,
+    'meshagent_x86-64_nokvm': 20,
+    'meshagent_arm-linaro': 24,
+    'meshagent_armhf': 25,
+    'meshagent_arm64': 26,
+    'meshagent_freebsd_x86-64': 30
+}
+
+for (var i in agents) { r += hashAgent(i, agents[i]); }
+console.log(r);
+process.exit();
+
+function hashAgent(filename, id) {
+    if (fs.existsSync(filename) != true) return '';
+    return id + ': ' + filename + '\r\n' + getSHA384FileHash(filename).toString('hex') + '\r\n';
+}
