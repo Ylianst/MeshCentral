f493832dd1f1daad6a8d270bb050c0509e636cf4
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2020-09-07 20:18:46 -0700

    Improved Spanish translation.

--- translate/translate.bat
+++ translate/translate.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js minifyall
-%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js translateall
-%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js extractall
+node translate.js minifyall
+node translate.js translateall
+node translate.js extractall
