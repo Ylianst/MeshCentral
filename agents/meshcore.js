2d8214d1df62973ab58be5f5b1eef07881f65351
Bryan Roe <git.bryan.roe@techsavvydude.com>
2021-01-16 13:12:30 -0800

    Removed debug lines that were accidentally left in

--- agents/meshcore.js
+++ agents/meshcore.js
@@ -4320,8 +4320,6 @@ function createMeshCore(agent)
             sendAgentMessage('Self Update failed because msvcrt.dll is missing', 3);
             return;
         }
-        var name = 'Mesh Agent';
-        var agentfilename = 'MeshAgent.exe';
 
         var cmd = require('_GenericMarshal').CreateVariable(process.env['windir'] + '\\system32\\cmd.exe', { wide: true });
         var args = require('_GenericMarshal').CreateVariable(3 * require('_GenericMarshal').PointerSize);
