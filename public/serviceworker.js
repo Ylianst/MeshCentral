ddf78e325c0f3d4877933220ea24f531a781ed37
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2021-02-10 01:31:52 -0800

    Added webpush to advanced config and schema.

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,12 +2,12 @@
 self.addEventListener('push', function (event) {
     if (event.data == null) return;
     var json = event.data.json();
-    const options = { body: json.body, icon: '/favicon-303x303.png', tag: json.url };
+    const options = { body: json.body, icon: '/favicon-303x303.png', data: { url: json.url } };
     if (json.icon) { options.icon = '/images/notify/icons128-' + json.icon + '.png'; }
-    self.registration.showNotification(json.title, options);
+    event.waitUntil(self.registration.showNotification(json.title, options));
 });
 
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
-    if ((event.notification.tag != null) && (event.notification.tag != '')) { event.waitUntil(self.clients.openWindow(event.notification.tag)); }
+    if ((event.notification.data.url != null) && (event.notification.data.url != '')) { event.waitUntil(self.clients.openWindow(event.notification.data.url)); }
 });
