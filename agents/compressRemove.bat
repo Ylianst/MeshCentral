2b3025716a2780ce1821c0129bc2e9aa7afcaa17
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2022-01-13 14:38:28 -0800

    Fixed meshcore cleanup .bat file.

--- agents/compressRemove.bat
+++ agents/compressRemove.bat
@@ -3,5 +3,6 @@ DEL meshcmd.min.js
 DEL meshcore.min.js
 DEL modules_meshcmd_min\*.min.js
 DEL modules_meshcore_min\*.min.js
+DEL modules_meshcore_min\*.json
 RD modules_meshcmd_min
 RD modules_meshcore_min
