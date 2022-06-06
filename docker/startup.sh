a994b298a402210371770c4eb20a06d25bfeac19
Simon Schön <sschoen@acecom.de>
2022-06-06 19:39:36 +0200

    use meshcentral files from local repository instead of the npm module

--- docker/startup.sh
+++ docker/startup.sh
@@ -11,7 +11,7 @@ export WEBRTC
 
 if [ -f "meshcentral-data/config.json" ]
     then
-        node node_modules/meshcentral 
+        node meshcentral/meshcentral 
     else
         cp config.json.template meshcentral-data/config.json
         sed -i "s/\"cert\": \"myserver.mydomain.com\"/\"cert\": \"$HOSTNAME\"/" meshcentral-data/config.json
@@ -21,11 +21,10 @@ if [ -f "meshcentral-data/config.json" ]
         sed -i "s/\"minify\": true/\"minify\": \"$MINIFY\"/" meshcentral-data/config.json
         sed -i "s/\"WebRTC\": false/\"WebRTC\": \"$WEBRTC\"/" meshcentral-data/config.json
         sed -i "s/\"AllowFraming\": false/\"AllowFraming\": \"$IFRAME\"/" meshcentral-data/config.json
-        if [ "$REVERSE_PROXY" != "false" ]
-            then 
-                sed -i "s/\"_certUrl\": \"my\.reverse\.proxy\"/\"certUrl\": \"https:\/\/$REVERSE_PROXY:$REVERSE_PROXY_TLS_PORT\"/" meshcentral-data/config.json
-                node node_modules/meshcentral
-                exit
+        if [ "$REVERSE_PROXY" != "false" ]; then
+            sed -i "s/\"_certUrl\": \"my\.reverse\.proxy\"/\"certUrl\": \"https:\/\/$REVERSE_PROXY:$REVERSE_PROXY_TLS_PORT\"/" meshcentral-data/config.json
+            node meshcentral/meshcentral
+            exit
         fi
-        node node_modules/meshcentral --cert "$HOSTNAME"     
+        node meshcentral/meshcentral --cert "$HOSTNAME"
 fi
