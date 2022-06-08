ea7e98b3b4f61268811a956d9d9e20a398673728
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2022-06-08 10:47:23 -0700

    Added BREACH attack mittigation, #4084

--- public/translate.bat
+++ public/translate.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 CD ..\translate
 %LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js minifyall
-%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js translateall
-%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js extractall
+REM %LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js translateall
+REM %LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js extractall
 DEL ..\emails\translations\*-min_*
 Pause
