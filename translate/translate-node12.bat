b2fbcdd27acd029ccabd9604ffe4e3b40304bd21
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2022-06-29 12:16:40 -0700

    Fixed UI glitch, #4192

--- translate/translate-node12.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js minifyall
-%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js translateall
-%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js extractall
--- translate/translate-node14.bat
+++ translate/translate-node14.bat
@@ -1,7 +1,4 @@
 @ECHO OFF
-CD ..\translate
 %LOCALAPPDATA%\..\Roaming\nvm\v14.16.0\node translate.js minifyall
 %LOCALAPPDATA%\..\Roaming\nvm\v14.16.0\node translate.js translateall
 %LOCALAPPDATA%\..\Roaming\nvm\v14.16.0\node translate.js extractall
-DEL ..\emails\translations\*-min_*
-Pause
