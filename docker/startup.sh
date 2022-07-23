558fe0ad6419db584a45782b919f7b7de0690ebe
Simon Schön <sschoen@acecom.de>
2022-07-23 13:22:55 +0200

    [docker] added SESSION_KEY environment variable for initial configuration

--- docker/startup.sh
+++ docker/startup.sh
@@ -15,6 +15,10 @@ if [ -f "meshcentral-data/${CONFIG_FILE}" ]
         sed -i "s/\"minify\": true/\"minify\": $MINIFY/" meshcentral-data/${CONFIG_FILE}
         sed -i "s/\"WebRTC\": false/\"WebRTC\": $WEBRTC/" meshcentral-data/${CONFIG_FILE}
         sed -i "s/\"AllowFraming\": false/\"AllowFraming\": $IFRAME/" meshcentral-data/${CONFIG_FILE}
+        if [ -z "$SESSION_KEY" ]; then
+            SESSION_KEY="$(cat /dev/urandom | tr -dc 'A-Za-z0-9!#$%&()*+,-./:;<=>?@[\]^_`{|}~' | fold -w 32 | head -n 1)";
+        fi
+        sed -i "s/\"_sessionKey\": \"MyReallySecretPassword1\"/\"sessionKey\": \"$SESSION_KEY\"/" meshcentral-data/${CONFIG_FILE}
         if [ "$REVERSE_PROXY" != "false" ]; then
             sed -i "s/\"_certUrl\": \"my\.reverse\.proxy\"/\"certUrl\": \"https:\/\/$REVERSE_PROXY:$REVERSE_PROXY_TLS_PORT\"/" meshcentral-data/${CONFIG_FILE}
             node meshcentral/meshcentral --configfile ${CONFIG_FILE}
