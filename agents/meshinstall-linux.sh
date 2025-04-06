70bed35aa45593622e877d1ba408f1dbf962fdce
si458 <simonsmith5521@gmail.com>
2025-04-06 17:15:12 +0100

    fix riscv64 in ‘meshinstall.sh’.sh
    Signed-off-by: si458 <simonsmith5521@gmail.com>

--- agents/meshinstall-linux.sh
+++ agents/meshinstall-linux.sh
@@ -114,7 +114,7 @@ CheckInstallAgent() {
           # RaspberryPi 3B+ running Ubuntu 64 (aarch64)
           machineid=26
         fi
-        if [ $marchinetype == 'riscv64' ]
+        if [ $machinetype == 'riscv64' ]
         then
           # RISC-V 64 bit
           machineid=45
