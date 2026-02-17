ab21b5724da4680b0de84188ec2a0dfa9c496c8f
Nick Szittai <nszittai@gmail.com>
2026-02-17 13:59:08 -0500

    revert: move isPrivateAddress from  common.js back to webserver.js

--- common.js
+++ common.js
@@ -17,7 +17,6 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const path = require('path');
-const net = require('net');
 const { URL } = require('url');
 
 // Binary encoding and decoding functions
@@ -217,34 +216,6 @@ module.exports.validateEmailDomain = function(email, allowedDomains) {
     }
 
     return true;
-}
-// Return true if this is a private/reserved IP address, using the ipcheck package (already a project dependency)
-module.exports.isPrivateIp = function (ip_addr) {
-    if (!ip_addr) return true;
-    ip_addr = String(ip_addr);
-
-    // If this is a loopback address, return true
-    if ((ip_addr == '127.0.0.1') || (ip_addr == '::1')) return true;
-
-    // Check IPv4 private addresses
-    const ipcheck = require('ipcheck');
-    const IPv4PrivateRanges = ['0.0.0.0/8', '10.0.0.0/8', '100.64.0.0/10', '127.0.0.0/8', '169.254.0.0/16', '172.16.0.0/12', '192.0.0.0/24', '192.0.0.0/29', '192.0.0.8/32', '192.0.0.9/32', '192.0.0.10/32', '192.0.0.170/32', '192.0.0.171/32', '192.0.2.0/24', '192.31.196.0/24', '192.52.193.0/24', '192.88.99.0/24', '192.168.0.0/16', '192.175.48.0/24', '198.18.0.0/15', '198.51.100.0/24', '203.0.113.0/24', '240.0.0.0/4', '255.255.255.255/32']
-    for (var i in IPv4PrivateRanges) { if (ipcheck.match(ip_addr, IPv4PrivateRanges[i])) return true; }
-
-    // Check IPv6 private addresses
-    return /^::$/.test(ip_addr) ||
-        /^::1$/.test(ip_addr) ||
-        /^::f{4}:([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/.test(ip_addr) ||
-        /^::f{4}:0.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/.test(ip_addr) ||
-        /^64:ff9b::([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/.test(ip_addr) ||
-        /^100::([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
-        /^2001::([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
-        /^2001:2[0-9a-fA-F]:([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
-        /^2001:db8:([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
-        /^2002:([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
-        /^f[c-d]([0-9a-fA-F]{2,2}):/i.test(ip_addr) ||
-        /^fe[8-9a-bA-B][0-9a-fA-F]:/i.test(ip_addr) ||
-        /^ff([0-9a-fA-F]{2,2}):/i.test(ip_addr)
 }
 
 // Validate that a string is a parseable http or https URL with a hostname
--- webserver.js
+++ webserver.js
@@ -10350,7 +10350,30 @@ module.exports.CreateWebServer = function (parent, db, args, certificates, doneF
     }
 
     // Return true if this is a private IP address
-    function isPrivateAddress(ip_addr) { return obj.common.isPrivateIp(ip_addr); }
+    function isPrivateAddress(ip_addr) {
+        // If this is a loopback address, return true
+        if ((ip_addr == '127.0.0.1') || (ip_addr == '::1')) return true;
+
+        // Check IPv4 private addresses
+        const ipcheck = require('ipcheck');
+        const IPv4PrivateRanges = ['0.0.0.0/8', '10.0.0.0/8', '100.64.0.0/10', '127.0.0.0/8', '169.254.0.0/16', '172.16.0.0/12', '192.0.0.0/24', '192.0.0.0/29', '192.0.0.8/32', '192.0.0.9/32', '192.0.0.10/32', '192.0.0.170/32', '192.0.0.171/32', '192.0.2.0/24', '192.31.196.0/24', '192.52.193.0/24', '192.88.99.0/24', '192.168.0.0/16', '192.175.48.0/24', '198.18.0.0/15', '198.51.100.0/24', '203.0.113.0/24', '240.0.0.0/4', '255.255.255.255/32']
+        for (var i in IPv4PrivateRanges) { if (ipcheck.match(ip_addr, IPv4PrivateRanges[i])) return true; }
+
+        // Check IPv6 private addresses
+        return /^::$/.test(ip_addr) ||
+            /^::1$/.test(ip_addr) ||
+            /^::f{4}:([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/.test(ip_addr) ||
+            /^::f{4}:0.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/.test(ip_addr) ||
+            /^64:ff9b::([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/.test(ip_addr) ||
+            /^100::([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
+            /^2001::([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
+            /^2001:2[0-9a-fA-F]:([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
+            /^2001:db8:([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
+            /^2002:([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/.test(ip_addr) ||
+            /^f[c-d]([0-9a-fA-F]{2,2}):/i.test(ip_addr) ||
+            /^fe[8-9a-bA-B][0-9a-fA-F]:/i.test(ip_addr) ||
+            /^ff([0-9a-fA-F]{2,2}):/i.test(ip_addr)
+    }
 
     // Check that a cookie IP is within the correct range depending on the active policy
     function checkCookieIp(cookieip, ip) {
