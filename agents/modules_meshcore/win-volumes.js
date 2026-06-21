2331a7590b3ce6c374f28cfef3a13f83e0a1749f
PTR <47147771+PTR-inc@users.noreply.github.com>
2026-06-21 14:30:35 +0200

    Win-wmi-fixed: add wmi exec method (#7910)
    * win-wmi-fixed: add wmi execMethod function
    
    * win-volumes: add win_volumes_wmi as replacement for win_volumes
    Uses wmi methods to get the identifier & recoverykey instead of manage-bde.
    Also adds win7 bitlocker support
    
    * Apply suggestion from @si458
    
    Co-authored-by: Simon Smith <simonsmith5521@gmail.com>
    
    ---------
    
    Co-authored-by: Simon Smith <simonsmith5521@gmail.com>

--- agents/modules_meshcore/win-volumes.js
+++ agents/modules_meshcore/win-volumes.js
@@ -111,7 +111,98 @@ function windows_volumes()
     return (p1);
 }
 
-module.exports = { 
+// Same as windows_volumes() but get the BitLocker recovery password through wmi. And win7 added
+function windows_volumes_wmi()
+{
+    var promise = require('promise');
+    var wmi = require('win-wmi-fixed');
+    const NS_VE = 'ROOT\\CIMV2\\Security\\MicrosoftVolumeEncryption';
+    var p1 = new promise(function (res, rej) { this._res = res; this._rej = rej; });
+    var drives = {};
+    var statuses = {
+        0: 'FullyDecrypted',
+        1: 'FullyEncrypted',
+        2: 'EncryptionInProgress',
+        3: 'DecryptionInProgress',
+        4: 'EncryptionPaused',
+        5: 'DecryptionPaused'
+    };
+
+    try {
+        // use win32_volume, for future property expansion and link through DeviceID
+        wmi.query('ROOT\\CIMV2', 'SELECT DriveLetter,Label,FileSystem,Capacity,FreeSpace,DriveType FROM Win32_Volume', ['DriveLetter', 'Label', 'FileSystem', 'Capacity', 'FreeSpace', 'DriveType'], false, 5000)
+            .forEach(function (vol) {
+                if (!vol || vol['DriveLetter'] == null) { return; }   // skip letterless volumes for now
+                var drive = vol['DriveLetter'].slice(0, -1);
+                drives[drive] = {
+                    name: (vol['Label'] ? vol['Label'] : ""),
+                    type: (vol['FileSystem'] ? vol['FileSystem'] : "Unknown"),
+                    size: (vol['Capacity'] ? vol['Capacity'] : 0),
+                    sizeremaining: (vol['FreeSpace'] ? vol['FreeSpace'] : 0),
+                    removable: (vol['DriveType'] == 2),
+                    cdrom: (vol['DriveType'] == 5)
+                };
+            });
+
+        // The MicrosoftVolumeEncryption namespace is admin-only and manage-bde needs elevation; skip entirely when not elevated, saves waiting on the wmi-query time-out if run as user
+        if (require('user-sessions').isRoot()) {
+            // Check win version: Win7=7600/7601, Win8+>=9200.
+            // Default to win8+ if unreadable.
+            var win8plus = true;
+            try {
+                var reg = require('win-registry');
+                var build = parseInt(reg.QueryKey(reg.HKEY.LocalMachine, 'Software\\Microsoft\\Windows NT\\CurrentVersion', 'CurrentBuildNumber'), 10);
+                win8plus = !(build > 0 && build < 9200);
+            } catch (ex) { }
+            // Win7 lacks the ConversionStatus property, so only select it on win8+
+            var q = win8plus ? 'SELECT DeviceID,DriveLetter,ConversionStatus,ProtectionStatus FROM Win32_EncryptableVolume' : 'SELECT DeviceID,DriveLetter,ProtectionStatus FROM Win32_EncryptableVolume';
+            var fields = win8plus ? ['DeviceID', 'DriveLetter', 'ConversionStatus', 'ProtectionStatus'] : ['DeviceID', 'DriveLetter', 'ProtectionStatus'];
+            // re-use connection instead of creating a new one every time
+            var sess = wmi.connect(NS_VE);
+            try {
+                wmi.query(NS_VE, q, fields, false, 5000)
+                    .forEach(function (vol) {
+                        if (!vol || vol['DriveLetter'] == null) { return; }   // shortcut letterless volumes
+                        var drive = vol['DriveLetter'].slice(0, -1);
+                        if (!drives[drive]) { return; }                // no matching volume, skip for now. TODO match op DeviceID
+                        try {
+                            var v_id = { DeviceID: vol['DeviceID'] };   // DeviceID(=VolumeKeyProtectorID) is the key needed for the ExecMethod calls
+                            var conv;
+                            if (win8plus) {
+                                conv = vol.ConversionStatus;            // property available from the query
+                            } else {
+                                var cs = sess.execMethod('Win32_EncryptableVolume', v_id, 'GetConversionStatus', null, 5000);   // win7: no PrecisionFactor param, so no universal get
+                                conv = (cs && cs.ReturnValue == 0) ? cs.ConversionStatus : 0;
+                            }
+                            // convert status to string.
+                            drives[drive].volumeStatus = statuses.hasOwnProperty(conv) ? statuses[conv] : 'FullyDecrypted';
+                            drives[drive].protectionStatus = (vol.ProtectionStatus == 0 ? 'Off' : (vol.ProtectionStatus == 1 ? 'On' : 'Unknown'));
+                            // Only retrieve the recovery key on encrypted drives (conv 0 = FullyDecrypted, otherwise some sort of encryption)
+                            if (conv != 0) {
+                                // Get protectorKey (= identifier). For now only { KeyProtectorType: 3 } (=numerical password=recoverykey). Future possibilities: get all id's and statuses (TPM , Certificate etc)
+                                // https://learn.microsoft.com/en-us/windows/win32/secprov/getkeyprotectors-win32-encryptablevolume
+                                var protKeys = sess.execMethod('Win32_EncryptableVolume', v_id, 'GetKeyProtectors', { KeyProtectorType: 3 }, 5000);
+                                if (protKeys && protKeys.ReturnValue == 0 && Array.isArray(protKeys.VolumeKeyProtectorID) && protKeys.VolumeKeyProtectorID.length > 0) {
+                                    drives[drive].identifier = protKeys.VolumeKeyProtectorID[0];
+                                    // Got an id, use it to retrieve the recoverykey
+                                    // https://learn.microsoft.com/en-us/windows/win32/secprov/getkeyprotectornumericalpassword-win32-encryptablevolume
+                                    var numPass = sess.execMethod('Win32_EncryptableVolume', v_id, 'GetKeyProtectorNumericalPassword', { VolumeKeyProtectorID: protKeys.VolumeKeyProtectorID[0] }, 5000);
+                                    // NumericalPassword is null if the volume is locked
+                                    if (numPass && numPass.ReturnValue == 0 && numPass.NumericalPassword) { drives[drive].recoveryPassword = numPass.NumericalPassword; }
+                                }
+                            }
+                        } catch (ex) { console.log('win-volumes bitlocker error for ' + drive + ': ' + (ex && ex.message ? ex.message : ex)); } // skip this volume, carry on
+                    });
+            } finally { sess.release(); }
+        }
+        console.log(JSON.stringify(drives, null, 1));
+        p1._res(drives);
+    } catch (e) { console.log('windows_volumes error: ' + (e && e.message ? e.message : e)); p1._res(drives); } // just return what we got
+
+    return (p1);
+}
+
+module.exports = {
     getVolumes: function () { try { return (getVolumes()); } catch (x) { return ({}); } },
-    volumes_promise: windows_volumes
+    volumes_promise: windows_volumes_wmi
 };
