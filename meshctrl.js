026e7c7a00c3c5039d4a6d425df7363cc0f2e47e
stephannn <38951729+stephannn@users.noreply.github.com>
2026-07-26 18:52:35 +0200

    Add files via upload

--- meshctrl.js
+++ meshctrl.js
@@ -1669,6 +1669,7 @@ function serverConnect() {
                 if (args.nofiles) { meshrights |= 1024; }
                 if (args.noregistry) { meshrights |= 4194304; }
                 if (args.nosoftware) { meshrights |= 8388608; }
+                if (args.softwareread) { meshrights |= 16777216; }
                 if (args.noamt) { meshrights |= 2048; }
                 if (args.limiteddesktop) { meshrights |= 4096; }
                 if (args.limitedevents) { meshrights |= 8192; }
@@ -1698,6 +1699,7 @@ function serverConnect() {
                 if (args.nofiles) { meshrights |= 1024; }
                 if (args.noregistry) { meshrights |= 4194304; }
                 if (args.nosoftware) { meshrights |= 8388608; }
+                if (args.softwareread) { meshrights |= 16777216; }
                 if (args.noamt) { meshrights |= 2048; }
                 if (args.limiteddesktop) { meshrights |= 4096; }
                 if (args.limitedevents) { meshrights |= 8192; }
